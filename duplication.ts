a84d590f9d42e6ad7d33290364baa541bde7ff3b
Dave Henton <dave@qlty.sh>
2026-02-25 11:34:59 -0600

    Update duplication.ts

--- duplication.ts
+++ duplication.ts
@@ -20,3 +20,26 @@ if
             
 setTimeout
 (cb, explicitEnterDuration)
+
+
+     if (expectsCSS) {
+    addTransitionClass(el, startClass)
+    addTransitionClass(el, activeClass)
+    nextFrame(() => {
+      removeTransitionClass(el, startClass)
+      // @ts-expect-error
+      if (!cb.cancelled) {
+        addTransitionClass(el, toClass)
+        if (!userWantsControl) {
+          if (isValidDuration(explicitEnterDuration)) {
+            setTimeout(cb, explicitEnterDuration)
+          } else {
+            whenTransitionEnds(el, type, cb)
+          }
+        }
+      }
+    })
+  }
+
+  if (vnode.data.show) {
+    toggleDisplay && toggleDisplay()
